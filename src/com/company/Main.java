3d9ca07679df2c05dff7208e68faa305f3b3b05a
tsapkojava <tsapko.java@gmail.com>
2021-04-14 17:30:37 +0300

    added new class - user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello man!");
+        User user = new User("Roma", 30);
+        System.out.println(user);
 
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class User {
+    String name;
+    int age;
+}
